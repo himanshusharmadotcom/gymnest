19f4e38ee2284b4ae168291d718d885b58bc6148
Himanshu <hs7772043@gmail.com>
2024-08-07 11:39:33 +0530

    install react js, tailwind css, material ui and created the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='text-3xl text-red-500'>App</div>
+      <Button variant='contained'>Hello World</Button>
+    </>
+  )
+}
+
+export default App
